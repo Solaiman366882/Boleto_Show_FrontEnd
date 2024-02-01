813911f3486fadd0bfa8a47d920c73c47c562741
Md.Solaiman <md.solaiman366882@gmail.com>
2024-02-01 18:09:43 +0600

    Ticket Section Design Completed

--- src/Components/ShowDetails/TicketSection/TicketSection.jsx
+++ src/Components/ShowDetails/TicketSection/TicketSection.jsx
@@ -1,15 +1,109 @@
-import { Container } from "react-bootstrap";
+import { Button, Container, Modal } from "react-bootstrap";
+import PropTypes from "prop-types";
+import { useState } from "react";
+import Form from 'react-bootstrap/Form';
 
-const TicketSection = () => {
+const TicketSection = ({ show }) => {
+	const [showModal, setShowModal] = useState(false);
+	const handleClose = () => setShowModal(false);
+	const handleShow = () => setShowModal(true);
+
 	return (
-		<div className="ticket-section">
+		<div className="ticket-section section-padding">
 			<Container>
-				<div className="ticket-area">
-                    
-                </div>
+				<div className="ticket-area  d-flex justify-content-between gap-4 align-items-start">
+					<div className="summery">
+						<h2>Summery</h2>
+						<p>{show.summary}</p>
+					</div>
+					<button className="c-btn" onClick={handleShow}>
+						Book Tickets
+					</button>
+				</div>
 			</Container>
+			<div className="mmodal">
+				<Modal
+					show={showModal}
+					onHide={handleClose}
+					size="lg"
+					aria-labelledby="contained-modal-title-vcenter"
+					centered
+				>
+					<Modal.Header closeButton>
+						<Modal.Title id="contained-modal-title-vcenter">
+							Book a Movie Ticket
+						</Modal.Title>
+					</Modal.Header>
+					<Modal.Body>
+						<div className="form-area">
+							<form action="">
+								<Form>
+									<Form.Group
+										className="mb-3"
+										controlId="exampleForm.ControlInput1"
+									>
+										<Form.Label>Movie Name</Form.Label>
+										<Form.Control
+											type="text"
+											placeholder={show.name}
+                                            value={`${show.name} Movie`}
+                                            readOnly
+										/>
+									</Form.Group>
+									<Form.Group
+										className="mb-3"
+										controlId="exampleForm.ControlInput1"
+									>
+										<Form.Label>Your Name</Form.Label>
+										<Form.Control
+											type="text"
+											placeholder="Enter Your Name"
+										/>
+									</Form.Group>
+									<Form.Group
+										className="mb-3"
+										controlId="exampleForm.ControlInput1"
+									>
+										<Form.Label>Email address</Form.Label>
+										<Form.Control
+											type="email"
+											placeholder="Enter Your Email"
+										/>
+									</Form.Group>
+									<Form.Group
+										className="mb-3"
+										controlId="exampleForm.ControlInput1"
+									>
+										<Form.Label>Your Address</Form.Label>
+										<Form.Control
+											type="text"
+											placeholder="Enter Your Address"
+										/>
+									</Form.Group>
+									<Form.Group
+										className="mb-3"
+										controlId="exampleForm.ControlTextarea1"
+									>
+										<Form.Label>
+											Give us a Feedback
+										</Form.Label>
+										<Form.Control as="textarea" rows={3} />
+									</Form.Group>
+								</Form>
+							</form>
+						</div>
+					</Modal.Body>
+					<Modal.Footer>
+						<Button onClick={handleClose}>Close</Button>
+					</Modal.Footer>
+				</Modal>
+			</div>
 		</div>
 	);
+};
+
+TicketSection.propTypes = {
+	show: PropTypes.object,
 };
 
 export default TicketSection;
--- src/Pages/ShowDetails/ShowDetails.jsx
+++ src/Pages/ShowDetails/ShowDetails.jsx
@@ -1,11 +1,13 @@
 import { useLoaderData } from "react-router-dom";
 import ShowDetailsBanner from "../../Components/ShowDetails/ShowDetails/ShowDetailsBanner";
+import TicketSection from "../../Components/ShowDetails/TicketSection/TicketSection";
 
 const ShowDetails = () => {
     const show = useLoaderData() || {};
     return (
         <div>
             <ShowDetailsBanner show={show}></ShowDetailsBanner>
+            <TicketSection show={show}></TicketSection>
         </div>
     );
 };
