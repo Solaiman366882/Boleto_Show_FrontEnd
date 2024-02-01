77adf58e1e3259ef897ff496f4b8ec62f0b62f6c
Md.Solaiman <md.solaiman366882@gmail.com>
2024-02-01 22:45:49 +0600

    header section design completed

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,33 +1,141 @@
-import { Container, Nav, Navbar, Offcanvas } from "react-bootstrap";
+import { Container, Offcanvas } from "react-bootstrap";
+import logo from "../../assets/Images/logo.png";
+import { NavLink } from "react-router-dom";
+import { useState } from "react";
+import { FaBarsStaggered } from "react-icons/fa6";
 
 const Header = () => {
-    return (
-        <div className="header-section">
-            <Navbar expand="md" className="mb-3 navbar-transparent ">
-                <Container fluid>
-                    <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-                    <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-md`} />
-                    <Navbar.Offcanvas
-                        id={`offcanvasNavbar-expand-md`}
-                        aria-labelledby={`offcanvasNavbarLabel-expand-md`}
-                        placement="end"
-                    >
-                        <Offcanvas.Header closeButton>
-                            <Offcanvas.Title id={`offcanvasNavbarLabel-expand-md`}>
-                                Offcanvas
-                            </Offcanvas.Title>
-                        </Offcanvas.Header>
-                        <Offcanvas.Body>
-                            <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link href="#action1">Home</Nav.Link>
-                                <Nav.Link href="#action2">Link</Nav.Link>
-                            </Nav>
-                        </Offcanvas.Body>
-                    </Navbar.Offcanvas>
-                </Container>
-            </Navbar>
-        </div>
-    );
+	const [show, setShow] = useState(false);
+
+	const handleClose = () => setShow(false);
+	const handleShow = () => setShow(true);
+
+	return (
+		<div className="header-section">
+			<Container>
+				<nav className="menu-area">
+					<div className="logo-area">
+						<img src={logo} alt="" />
+					</div>
+					<div className="menu-btn">
+						<ul>
+							<li>
+								<NavLink
+									to="/"
+									className={({ isActive, isPending }) =>
+										isPending
+											? "pending"
+											: isActive
+											? "active"
+											: ""
+									}
+								>
+									home
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									to="/about"
+									className={({ isActive, isPending }) =>
+										isPending
+											? "pending"
+											: isActive
+											? "active"
+											: ""
+									}
+								>
+									about
+								</NavLink>
+							</li>
+							<li>
+								<NavLink
+									to="/about"
+									className={({ isActive, isPending }) =>
+										isPending
+											? "pending"
+											: isActive
+											? "active"
+											: ""
+									}
+								>
+									contact
+								</NavLink>
+							</li>
+						</ul>
+						<button className="c-btn">Join Us</button>
+					</div>
+					<div>
+						<button className="show-menu" onClick={handleShow}>
+							<FaBarsStaggered />
+						</button>
+						<Offcanvas show={show} onHide={handleClose}>
+							<Offcanvas.Header closeButton>
+								<Offcanvas.Title><img src={logo}></img></Offcanvas.Title>
+							</Offcanvas.Header>
+							<Offcanvas.Body>
+								<div className="menu-btn">
+									<ul>
+										<li>
+											<NavLink
+												to="/"
+												className={({
+													isActive,
+													isPending,
+												}) =>
+													isPending
+														? "pending"
+														: isActive
+														? "active"
+														: ""
+												}
+											>
+												home
+											</NavLink>
+										</li>
+										<li>
+											<NavLink
+												to="/about"
+												className={({
+													isActive,
+													isPending,
+												}) =>
+													isPending
+														? "pending"
+														: isActive
+														? "active"
+														: ""
+												}
+											>
+												about
+											</NavLink>
+										</li>
+										<li>
+											<NavLink
+												to="/about"
+												className={({
+													isActive,
+													isPending,
+												}) =>
+													isPending
+														? "pending"
+														: isActive
+														? "active"
+														: ""
+												}
+											>
+												contact
+											</NavLink>
+										</li>
+									</ul>
+									<button className="c-btn">Join Us</button>
+								</div>
+							</Offcanvas.Body>
+						</Offcanvas>
+					</div>
+				</nav>
+			</Container>
+		</div>
+	);
 };
 
 export default Header;
